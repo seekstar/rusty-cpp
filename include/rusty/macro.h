5cbb4f30e2bd5dbd9322adf5aa62b13dc642aa8a
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-04-23 22:20:33 +0800

    macro

---
+++ include/rusty/macro.h
@@ -0,0 +1,15 @@
+#ifndef RUSTY_MACRO_H_
+#define RUSTY_MACRO_H_
+
+#define rusty_panic(...) do { \
+    fprintf(stderr, "panic: %s:%u: %s:", \
+        __FILE__, __LINE__, __func__); \
+    fprintf(stderr, " " __VA_ARGS__);   \
+    abort(); \
+} while (0)
+
+#define rusty_assert(cond, ...) do { \
+    if (!cond) { rusty_panic(__VA_ARGS__); }   \
+} while (0)
+
+#endif // RUSTY_MACRO_H_
