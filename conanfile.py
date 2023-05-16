fb68f927a92ff816615842896bacd43cce0caf81
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-05-16 16:47:36 +0800

    conan

---
+++ conanfile.py
@@ -0,0 +1,44 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+
+class rusty_cppRecipe(ConanFile):
+    name = "rusty-cpp"
+    version = "0.1.0"
+
+    # Optional metadata
+    license = "MPLv2"
+    author = "Jiansheng Qiu jianshengqiu.cs@gmail.com"
+    url = "https://github.com/seekstar/rusty-cpp"
+    description = "Write C++ like rust"
+    topics = ("C++", "Rust")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "include/*"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        # For header-only packages, libdirs and bindirs are not used
+        # so it's necessary to set those as empty.
+        self.cpp_info.set_property("cmake_target_name", "rusty-cpp")
+        self.cpp_info.libdirs = []
+        self.cpp_info.bindirs = []
