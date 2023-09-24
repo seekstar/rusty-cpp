5e0ec7227a8e8d58d7d2826a15cdcbf57264a964
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-09-24 19:55:06 +0800

    ManuallyDrop

---
+++ include/rusty/mem.h
@@ -0,0 +1,32 @@
+#ifndef RUSTY_MEM_H_
+#define RUSTY_MEM_H_
+
+#include <utility>
+
+namespace rusty {
+namespace mem {
+
+template <typename T>
+class ManuallyDrop {
+public:
+	ManuallyDrop() {}
+	ManuallyDrop(const ManuallyDrop<T> &rhs) = delete;
+	ManuallyDrop &operator=(const ManuallyDrop<T> &rhs) = delete;
+	ManuallyDrop(ManuallyDrop<T> &&rhs) : v_(std::move(rhs.v_)) {}
+	ManuallyDrop<T> &operator=(ManuallyDrop<T> &&rhs) {
+		v_ = std::move(rhs.v_);
+		return *this;
+	}
+	ManuallyDrop &operator=(T &&v) { v_ = std::move(v); return *this; }
+	T into_inner() { return std::move(v_); }
+	void drop() { v_.~T(); }
+private:
+	union {
+		int v_;
+	};
+};
+
+} // namespace mem
+} // namespace rusty
+
+#endif // RUSTY_MEM_H_
