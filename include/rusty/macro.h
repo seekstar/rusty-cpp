be05ba980825959ebd8c83d8040c57e1047f1975
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-10-06 23:42:00 +0800

    rusty_assert_eq

--- include/rusty/macro.h
+++ include/rusty/macro.h
@@ -19,7 +19,29 @@
 } while (0)
 
 #define rusty_assert(cond, ...) do { \
-	if (!(cond)) { rusty_panic("Assertion `" #cond "` failed"); }   \
+	if (!(cond)) { \
+		rusty_panic("Assertion `" #cond "` failed. " __VA_ARGS__); \
+	} \
+} while (0)
+
+#define __rusty_first_arg(a, ...) a
+#define __rusty_drop_first_arg_with_prepended_comma(a, ...) , ## __VA_ARGS__
+
+#define rusty_assert_eq(l, r, ...) do { \
+	auto left= (l); \
+	auto right= (r); \
+	if (sizeof(#__VA_ARGS__) == 1) { \
+		rusty_assert( \
+			left == right, "left = %s, right = %s", \
+			std::to_string(left).c_str(), std::to_string(right).c_str() \
+		); \
+	} else { \
+		rusty_assert( \
+			left == right, "left = %s, right = %s. " __rusty_first_arg(__VA_ARGS__), \
+			std::to_string(left).c_str(), std::to_string(right).c_str() \
+			__rusty_drop_first_arg_with_prepended_comma(__VA_ARGS__) \
+		); \
+	} \
 } while (0)
 
 #endif // RUSTY_MACRO_H_
