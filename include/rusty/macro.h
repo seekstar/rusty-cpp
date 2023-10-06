d06155e794282e180f555fb42ce43c46fc21c75e
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-10-06 22:44:47 +0800

    Better rusty_panic and rusty_assert

--- include/rusty/macro.h
+++ include/rusty/macro.h
@@ -5,14 +5,21 @@
 #include <cstdlib>
 
 #define rusty_panic(...) do { \
-    fprintf(stderr, "panic: %s:%u: %s:", \
-        __FILE__, __LINE__, __func__); \
-    fprintf(stderr, " " __VA_ARGS__);   \
-    abort(); \
+	/* The "if" should be optimized out by the compiler */ \
+	if (sizeof(#__VA_ARGS__) == 1) { \
+		fprintf(stderr, "panic: %s:%u: %s\n", \
+			__FILE__, __LINE__, __func__); \
+	} else { \
+		fprintf(stderr, "panic: %s:%u: %s", \
+			__FILE__, __LINE__, __func__); \
+		fprintf(stderr, ": " __VA_ARGS__);   \
+		fprintf(stderr, "\n"); \
+	} \
+	abort(); \
 } while (0)
 
 #define rusty_assert(cond, ...) do { \
-    if (!(cond)) { rusty_panic(__VA_ARGS__); }   \
+	if (!(cond)) { rusty_panic("Assertion `" #cond "` failed"); }   \
 } while (0)
 
 #endif // RUSTY_MACRO_H_
