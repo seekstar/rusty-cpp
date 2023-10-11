f33b84d2da95e6489a2a21b120bb10974644a947
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-10-11 13:55:12 +0800

    rusty_assert_ne

--- include/rusty/macro.h
+++ include/rusty/macro.h
@@ -44,4 +44,21 @@
 	} \
 } while (0)
 
+#define rusty_assert_ne(l, r, ...) do { \
+	auto left= (l); \
+	auto right= (r); \
+	if (sizeof(#__VA_ARGS__) == 1) { \
+		rusty_assert( \
+			left != right, "left = %s, right = %s", \
+			std::to_string(left).c_str(), std::to_string(right).c_str() \
+		); \
+	} else { \
+		rusty_assert( \
+			left != right, "left = %s, right = %s. " __rusty_first_arg(__VA_ARGS__), \
+			std::to_string(left).c_str(), std::to_string(right).c_str() \
+			__rusty_drop_first_arg_with_prepended_comma(__VA_ARGS__) \
+		); \
+	} \
+} while (0)
+
 #endif // RUSTY_MACRO_H_
