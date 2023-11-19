4aa3b3d267fa0768bdd06d87a2a0f5345167a873
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-11-19 14:41:00 +0800

    v0.1.9

--- include/rusty/time.h
+++ include/rusty/time.h
@@ -2,6 +2,7 @@
 #define RUSTY_TIME_H_
 
 #include <chrono>
+#include <optional>
 
 namespace rusty {
 namespace time {
@@ -12,6 +13,11 @@ public:
 		nsec_ = rhs.nsec_;
 		return *this;
 	}
+	template <typename Rep, typename Period>
+	Duration(const std::chrono::duration<Rep, Period> &duration)
+	  : nsec_(std::chrono::duration_cast<std::chrono::nanoseconds>(
+			duration
+		).count()) {}
 	static Duration from_nanos(uint64_t nsec) { return Duration(nsec); }
 	uint64_t as_nanos() const {
 		return nsec_;
@@ -39,10 +45,23 @@ public:
 	Duration elapsed() const {
 		return now() - *this;
 	}
-	Duration operator-(const Instant& earlier) {
+	Duration operator-(const Instant &earlier) {
 		return Duration::from_nanos(
 			std::chrono::duration_cast<std::chrono::nanoseconds>(
 				time_ - earlier.time_).count());
+	}
+	Instant operator+(const Duration &duration) {
+		return time_ + std::chrono::nanoseconds(duration.as_nanos());
+	}
+	Instant &operator+=(const Duration &duration) {
+		time_ += std::chrono::nanoseconds(duration.as_nanos());
+		return *this;
+	}
+	std::optional<Duration> checked_duration_since(const Instant &earlier) {
+		if (time_ < earlier.time_) {
+			return std::nullopt;
+		}
+		return time_ - earlier.time_;
 	}
 private:
 	Instant(std::chrono::steady_clock::time_point time) : time_(time) {}
