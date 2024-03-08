a59f49ae1b6b8c6aad16586c5b866a8464960099
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-03-08 15:12:15 +0800

    Use forwarding reference in rusty::match

--- include/rusty/keyword.h
+++ include/rusty/keyword.h
@@ -3,13 +3,20 @@
 
 #include <variant>
 
+namespace rusty {
+
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 // explicit deduction guide (not needed as of C++20)
 template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
 template <typename Val, typename... Ts>
-auto match(Val val, Ts... ts) {
-	return std::visit(overloaded{ts...}, val);
+auto match(Val &&val, Ts... ts) {
+	return std::visit(overloaded{ts...}, std::forward<Val>(val));
 }
+
+} // namespace rusty
+
+// DEPRECATED
+using rusty::match;
 
 #endif // RUSTY_KEYWORD_H_
