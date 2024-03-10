1067bdae2c000065ad7536e60148a89a8d18c470
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-03-10 22:41:38 +0800

    is_power_of_two

--- include/rusty/primitive.h
+++ include/rusty/primitive.h
@@ -3,6 +3,34 @@
 
 #include <rusty/macro.h>
 
+namespace rusty {
+
+template <typename T>
+static inline T __lowbit(T x) {
+  return x & -x;
+}
+
+template <typename T>
+bool __is_power_of_two_uint(T x) {
+	return x == __lowbit(x);
+}
+
+static inline unsigned char is_power_of_two(unsigned char x) {
+	return __is_power_of_two_uint(x);
+}
+static inline unsigned short is_power_of_two(unsigned short x) {
+	return __is_power_of_two_uint(x);
+}
+static inline unsigned int is_power_of_two(unsigned int x) {
+	return __is_power_of_two_uint(x);
+}
+static inline unsigned long is_power_of_two(unsigned long x) {
+	return __is_power_of_two_uint(x);
+}
+static inline unsigned long long is_power_of_two(unsigned long long x) {
+	return __is_power_of_two_uint(x);
+}
+
 // next_power_of_two
 // Returns the smallest power of two greater than or equal to "x".
 // Panics if overflow.
@@ -32,5 +60,10 @@ static inline unsigned long next_power_of_two(unsigned long x) {
 static inline unsigned long long next_power_of_two(unsigned long long x) {
 	return __next_power_of_two_uint(x);
 }
+
+}  // namespace rusty
+
+// DEPRECATED
+using rusty::next_power_of_two;
 
 #endif // RUSTY_PRIMITIVE_H_
