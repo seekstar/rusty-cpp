9dfef0c68e7383312b12e261d61f92d0b0332baa
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2025-07-18 11:04:09 +0800

    Usage for match

--- include/rusty/keyword.h
+++ include/rusty/keyword.h
@@ -9,6 +9,18 @@ template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 // explicit deduction guide (not needed as of C++20)
 template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
+/// Usage:
+/// match(value, [](Type1 type1) {
+/// 		;
+/// 	},
+/// 	[](Type2 type2) {
+/// 		;
+/// 	},
+/// 	...
+/// 	[](TypeN typeN) {
+/// 		;
+/// 	}
+/// );
 template <typename Val, typename... Ts>
 auto match(Val &&val, Ts... ts) {
 	return std::visit(overloaded{ts...}, std::forward<Val>(val));
