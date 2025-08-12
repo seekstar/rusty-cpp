f48b20d66a59d8d260b6cfe75fe7572eb9ee6ee6
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2025-08-12 18:19:14 +0800

    Test iterator trait object

--- include/rusty/iter/iterator.h
+++ include/rusty/iter/iterator.h
@@ -4,6 +4,7 @@
 #include "rusty/option.h"
 
 #include <functional>
+#include <memory>
 #include <vector>
 
 namespace rusty {
@@ -22,12 +23,60 @@ public:
 	Option<value_type> next() {
 		return next(type_tag_t<Iterator<value_type>>());
 	}
+
+	template <typename I>
+	class FatPointer;
 };
 
+template <typename T>
 template <typename I>
-void collect_into(I iter, std::vector<typename I::value_type> &v) {
+class Iterator<T>::FatPointer : public Iterator<T> {
+public:
+	explicit FatPointer(I &&iter) : iter_(std::move(iter)) {}
+	Option<T> next(type_tag_t<Iterator<T>>) override {
+		return iter_.next(type_tag_t<Iterator<T>>());
+	}
+
+private:
+	I iter_;
+};
+
+namespace detail {
+
+template <typename T>
+class IteratorImpl : public T {
+public:
+	template <typename... Args>
+	IteratorImpl(Args &&... args) : T(std::forward<Args>(args)...) {}
+};
+
+template <typename Iter>
+class IteratorImpl<std::unique_ptr<Iter>> {
+public:
+	using value_type = typename Iter::value_type;
+	explicit IteratorImpl(
+		std::unique_ptr<Iter> iter
+	) : iter_(std::move(iter)) {}
+	Option<value_type> next(type_tag_t<Iterator<value_type>>) {
+		return iter_->next();
+	}
+
+private:
+	std::unique_ptr<Iter> iter_;
+};
+
+} // namespace detail
+
+template <typename I>
+void collect_into(
+	I iter,
+	std::vector<typename detail::IteratorImpl<I>::value_type> &v
+) {
+	auto it = detail::IteratorImpl<I>(std::move(iter));
 	for (;;) {
-		auto res = iter.next(type_tag_t<Iterator<typename I::value_type>>());
+		auto res = it.next(
+			type_tag_t<Iterator<typename detail::IteratorImpl<I>::value_type>>()
+		);
 		if (res.is_none()) {
 			break;
 		}
@@ -60,6 +109,13 @@ private:
 template <typename T>
 Iter<T> MakeIter(const T *start, const T *end) {
 	return Iter<T>(start, end);
+}
+
+template <typename T>
+std::unique_ptr<Iterator<Ref<const T>>> MakeTraitObject(Iter<T> iter) {
+	return std::make_unique<
+		typename Iterator<Ref<const T>>::template FatPointer<Iter<T>>
+	>(std::move(iter));
 }
 
 } // namespace rusty
--- test_package/iterator_test.cpp
+++ test_package/iterator_test.cpp
@@ -3,14 +3,28 @@
 
 #include <gtest/gtest.h>
 
+namespace {
+void check(const std::vector<int> &a, const std::vector<rusty::Ref<const int>> &b) {
+	ASSERT_EQ(a.size(), b.size());
+	for (size_t i = 0; i < a.size(); ++i) {
+		ASSERT_EQ(a[i], b[i].deref());
+	}
+}
+}
+
 TEST_F(Test, IteratorSimple) {
 	std::vector<int> a{1, 3, 8, 2};
 	ASSERT_TRUE(rusty::MakeIter(a.data(), a.data()).next().is_none());
 
 	std::vector<rusty::Ref<const int>> b;
 	rusty::collect_into(rusty::MakeIter(a.data(), a.data() + a.size()), b);
-	ASSERT_EQ(a.size(), b.size());
-	for (size_t i = 0; i < a.size(); ++i) {
-		ASSERT_EQ(a[i], b[i].deref());
-	}
+	ASSERT_NO_FATAL_FAILURE(check(a, b));
+
+	auto iter = rusty::MakeTraitObject(rusty::MakeIter(a.data(), a.data()));
+	ASSERT_TRUE(iter->next().is_none());
+
+	iter = rusty::MakeTraitObject(rusty::MakeIter(a.data(), a.data() + a.size()));
+	b.clear();
+	rusty::collect_into(std::move(iter), b);
+	ASSERT_NO_FATAL_FAILURE(check(a, b));
 }
